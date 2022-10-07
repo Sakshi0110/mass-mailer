2f0785ca289318eb9c09897522ddcb4293e4fbd7
Sakshi Sindhuja <sakshisindhuja.01@gmail.com>
2022-10-07 16:42:05 +0530

    Added visibility modes to valid/invalid emails and final submit
    Hosted the full thing on tiiny.host, limited email limit to 100 coz gmail can't accept more than that.

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 document.getElementById("submitBtn").addEventListener("click", async function () {
-    const emailCSVContents = await document.getElementById("fileBtn").files.item(0).text()
+    let emailCSVContents = await document.getElementById("fileBtn").files.item(0).text()
+    emailCSVContents = emailCSVContents.replaceAll("\r", "")
     const emailList = emailCSVContents.trim().split('\n')
     emailList.shift()
+    if (emailList.length !== 0) {
+        document.getElementById("validInvalid").style.visibility = 'visible'
+        document.getElementById("continue").style.visibility = 'visible'
+    }
     let correctEmails = [];
     for (let emailId of emailList) {
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailId)) {
             document.getElementById("valid").innerHTML += "<p>" + emailId + "</p>";
-            correctEmails.push(emailId);
+            if (correctEmails.length < 100)
+                correctEmails.push(emailId);
         }
         else {
             document.getElementById("invalid").innerHTML += "<p>" + emailId + "</p>";
